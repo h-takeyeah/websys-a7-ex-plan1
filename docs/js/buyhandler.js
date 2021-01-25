bf4278d8e273612905a62777ee39a13deaf68300
h-takeyeah <61489178+h-takeyeah@users.noreply.github.com>
2021-01-25 23:02:04 +0900

    Move etc to public

--- docs/js/buyhandler.js
+++ docs/js/buyhandler.js
@@ -1,2 +1,2 @@
 const btn = document.getElementById('description').querySelector('a')
-btn.setAttribute('href', `${document.location.origin}/buy/item-${btn.dataset.item_number}.html`)
+btn.setAttribute('href', `../buy/item-${btn.dataset.item_number}.html`)
