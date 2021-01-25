5084bbdd6df8e4ebc07de7f6631414d4717c4e2e
h-takeyeah <61489178+h-takeyeah@users.noreply.github.com>
2021-01-25 22:47:41 +0900

    Fix header link

--- docs/js/header-footer.js
+++ docs/js/header-footer.js
@@ -3,7 +3,7 @@ const header =
   '<div class="hero-body">' +
     '<div class="container">' +
       '<h1 class="title">' +
-        '<a href="../static/index.html">T書店</a>' +
+        '<a href="../index.html">T書店</a>' +
       '</h1>' +
       '<h2 class="subtitle">' +
         '本・DVD/BDレンタル・ゲームのT書店' +
